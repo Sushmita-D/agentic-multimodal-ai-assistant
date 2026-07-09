727fce960fb3af8ab44173fb260ccdc45b402721
sushmita <sushmitadai vajnya@gmail.com>
2026-07-09 22:48:44 +0530

    Fix embedded model

--- Backend/rag/embedder.py
+++ Backend/rag/embedder.py
@@ -1,11 +1,16 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+print("Loading embedding model...")
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+print("Embedding model loaded.")
 def create_embeddings(chunks):
 
+    print("Creating embeddings...")
+
     embeddings = model.encode(chunks)
+
+    print("Embeddings created.")
 
     return embeddings
