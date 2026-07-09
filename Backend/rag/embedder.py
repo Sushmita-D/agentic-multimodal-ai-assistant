767cab58d1fceac143404a303a618433f7b3e661
sushmita <sushmitadai vajnya@gmail.com>
2026-07-09 23:21:45 +0530

    Fix embedded model

--- Backend/rag/embedder.py
+++ Backend/rag/embedder.py
@@ -1,16 +1,29 @@
-from sentence_transformers import SentenceTransformer
+print("1 Upload started")
 
-print("Loading embedding model...")
+print("2 File saved")
+
+print("3 Creating document")
+
+print("4 PDF extraction")
+
+print("5 Chunking")
+
+print("6 Creating embeddings")
+
+print("7 Saving chunks")
+
+print("8 Upload completed")
+from sentence_transformers import SentenceTransformer
+import time
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-print("Embedding model loaded.")
 def create_embeddings(chunks):
-
-    print("Creating embeddings...")
+    print("Embedding started")
+    start = time.time()
 
     embeddings = model.encode(chunks)
 
-    print("Embeddings created.")
+    print(f"Embedding finished in {time.time()-start:.2f} sec")
 
     return embeddings
